41c97c1c5806757da84cf4e98cd02af3ad4a1325
dengfei2013 <dengfei_2013@163.com>
2018-12-11 13:03:44 +0800

    add a summary function

--- app.R
+++ app.R
@@ -11,19 +11,33 @@ ui <- dashboardPage(
       br(),
       br(),
       menuItem("ped文件ID转换",tabName = "a", icon = icon("caret-square-right")),
-      menuItem("计算近交系数和亲缘关系系数",tabName = "b", icon = icon("caret-square-right"))
+      menuItem("计算近交系数和亲缘关系系数",tabName = "b", icon = icon("caret-square-right")),
+      menuItem("表型数据汇总统计",tabName = "c", icon = icon("caret-square-right"))
       
     )),
   dashboardBody(
     tabItems(
     tabItem(tabName = "a",
-            fileInput("ped_file","上传ped文件",accept = ".ped"),                         
-            tableOutput('head1'),
-            fileInput("id_file","上传id文件",accept = ".csv"),
-            tableOutput("head2"),
+            fileInput("dat1_1","上传ped文件",accept = ".ped"),                         
+            tableOutput('head1_1'),
+            fileInput("dat1_2","上传id文件",accept = ".csv"),
+            tableOutput("head1_2"),
             h5("结果文件\n"),
-            downloadButton("downloadData", "下载处理后的ped文件")),
-    tabItem(tabName = "b")
+            downloadButton("down1", "下载处理后的ped文件")),
+    
+    tabItem(tabName = "b",
+            fileInput("dat2","上传三列系谱数据",accept = ".csv"),                         
+            tableOutput('dat2'),
+            h5("结果文件\n"),
+            downloadButton("down2", "下载近交系数及亲缘关系系数")
+            ),
+    
+    tabItem(tabName = "c",
+            fileInput("dat3","上次表型数据",accept = ".csv"),                         
+            tableOutput('head3'),
+            h5("结果文件\n"),
+            downloadButton("down3", "下载数据汇总报表html")
+            )
   )
 ))
 
@@ -31,32 +45,52 @@ options(shiny.maxRequestSize=3000*1024^2)
 server <- function(input, output) {
   
   # data = mtcars
-  d1 <- reactive({
-    inFile1 <- input$ped_file
+  d1_1 <- reactive({
+    inFile1 <- input$dat1_1
     if (is.null(inFile1)) return(NULL)
     fread(inFile1$datapath)
   })
   
-  d2 <- reactive({
-    inFile2 <- input$id_file
+  d1_2 <- reactive({
+    inFile2 <- input$dat1_2
     if (is.null(inFile2)) return(NULL)
     fread(inFile2$datapath)
   })
   
-  output$head1 <- renderTable({
-    dat1 <- d1()
+  d2 <- reactive({
+    inFile2 <- input$dat2
+    if (is.null(inFile2)) return(NULL)
+    fread(inFile2$datapath)
+  })
+  
+  d3 <- reactive({
+    inFile3 <- input$dat3
+    if (is.null(inFile3)) return(NULL)
+    fread(inFile3$datapath)
+  })
+  
+  output$head1_1 <- renderTable({
+    dat1 <- d1_1()
     dat1[1:5,1:5]
   })
   
   output$head2 <- renderTable({
-    dat2 <- d2()
+    dat2 <- d1_2()
     head(dat2)
   })
   
-  output$re <- renderTable({
-
+  output$head2 <- renderTable({
+    dat1 <- d2()
+    dat1[1:5,1:5]
   })
-  output$downloadData <- downloadHandler(
+  
+  output$head3 <- renderTable({
+    dat2 <- d3()
+    head(dat2)
+  })
+  
+
+  output$down1 <- downloadHandler(
     filename = function() {
       paste("data-", Sys.time(), ".ped", sep=" ")
     },
@@ -70,6 +104,21 @@ server <- function(input, output) {
       fwrite(re, file,sep = " ")
     }
   )
+  
+  output$down3 <- downloadHandler(
+    filename = function() {
+      paste('Data-summary', Sys.time(), sep = '.', 'html')
+    },
+    content = function(file) {
+      dat = d3()
+      src <- normalizePath('report1.Rmd')
+      owd <- setwd(tempdir())
+      on.exit(setwd(owd))
+      file.copy(src, 'report1.Rmd', overwrite = TRUE)
+      library(rmarkdown)
+      out <- render('report1.Rmd', html_document())
+      file.rename(out, file)
+    })
   
 }
 
